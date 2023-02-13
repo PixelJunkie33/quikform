2892f3d11ba2505e3410e8e49c6f2a836285a8e0
webdev_p <webdevp@email.com>
2023-02-13 10:02:25 -0500

    button update to sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { React } from 'react';
+import './index.css';
 import './App.css';
+import ContainerBox from './sidebarComponents';
 
 function App() {
   return (
-   <>
-   
-   </>
+    <div>
+      <ContainerBox/>
+    </div>
   );
 }
 
